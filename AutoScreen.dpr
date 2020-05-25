ff983a462f26e1358aaf2ad3c32fbb3516a36b3d
Artem <megabyte1024@ya.ru>
2020-05-25 15:54:29 +0300

    Prevent the launch of more than one instance of process

--- AutoScreen.dpr
+++ AutoScreen.dpr
@@ -4,7 +4,7 @@ program AutoScreen;
 {$R 'res\VERSIONINFO.res' 'res\VERSIONINFO.rc'}
 
 uses
-  Forms,
+  Forms, Windows, Dialogs,
   uAutoScreen in 'uAutoScreen.pas' {MainForm},
   uAbout in 'uAbout.pas' {AboutForm},
   uLocalization in 'uLocalization.pas',
@@ -12,10 +12,28 @@ uses
 
 //{$R *.res}
 
+var
+  MutexHandle: THandle;
+  //MutexName: PChar;
+const
+  MutexName = 'AutoScreenshotMutex';
 begin
+  // Prevent the launch of more than one instance of process
+  //MutexName := PChar(Application.ExeName);
+  MutexHandle := OpenMutex(MUTEX_ALL_ACCESS, False, MutexName);
+  if MutexHandle <> 0 then
+  begin
+    CloseHandle(MutexHandle);
+    MessageDlg('Program already running!', mtWarning, [mbOk], 0);
+    Exit;
+  end;
+  MutexHandle := CreateMutex(nil, false, MutexName);
+
   Application.Initialize;
   Application.Title := 'Auto Screenshot'; // ToDo: Also rename executable to AutoScreenshot.exe
   Application.CreateForm(TMainForm, MainForm);
   Application.ShowMainForm := False;
   Application.Run;
+
+  CloseHandle(MutexHandle);
 end.
