artem78/AutoScreenshot

commit f349cdf07e820610132ca3099c130afaf36fe14e

Change program title color in About dialog

uAbout.dfm
@@ -22,11 +22,13 @@ object AboutForm: TAboutForm
     Width = 176
     Height = 37
     Caption = 'Auto Screen'
+    Color = clNone
     Font.Charset = DEFAULT_CHARSET
-    Font.Color = clNavy
+    Font.Color = 8611623
     Font.Height = -32
     Font.Name = 'MS Sans Serif'
     Font.Style = []
+    ParentColor = False
     ParentFont = False
   end
   object VersionLabel: TLabel

commit 0b90238d9e79f216ceaa953ee31ca1854322c998

Fix program title background color in "About" dialog after it been broken in f349cdf

uAbout.dfm
@@ -25,13 +25,11 @@ object AboutForm: TAboutForm
     Width = 174
     Height = 36
     Caption = 'Auto Screen'
-    Color = clNone
     Font.Charset = DEFAULT_CHARSET
     Font.Color = 8611623
     Font.Height = -32
     Font.Name = 'Arial'
     Font.Style = []
-    ParentColor = False
     ParentFont = False
   end
   object VersionLabel: TTntLabel