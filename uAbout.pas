3dd74ae8679ac6a13c5a80d39a440920d0175bab
artem <megabyte1024@ya.ru>
2021-06-25 21:49:06 +0300

    Comment unused modules

--- uAbout.pas
+++ uAbout.pas
@@ -5,7 +5,7 @@ unit uAbout;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  {Windows, Messages,} SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, {ShellApi,} LCLIntf, ExtCtrls{, TntForms, TntStdCtrls, TntExtCtrls};
 
 type
