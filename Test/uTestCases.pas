e33dd0cd9b0481c35aa1cfcc22376358583d45c4
artem <megabyte1024@ya.ru>
2021-08-27 20:25:23 +0300

    [Debug] Unit tests migration
    from DUnit to FPCUnit

--- Test/uTestCases.pas
+++ Test/uTestCases.pas
@@ -1,12 +1,17 @@
 unit uTestCases;
 
+{$mode objfpc}{$H+}
+
 interface
 
 uses
-  TestFramework;
+  Classes, SysUtils, fpcunit, testutils, testregistry;
 
 type
-  TMyTestCase = class(TTestCase)
+
+  { TMyTestCase }
+
+  TMyTestCase= class(TTestCase)
   private
     function GetFormattedPath(Path: String): String;
   published
@@ -21,6 +26,8 @@ implementation
 
 uses uUtils, {SysUtils} DateUtils;
 
+{ TMyTestCase }
+
 function TMyTestCase.GetFormattedPath(Path: String): String;
 var
   DateTime: TDateTime;
@@ -31,66 +38,67 @@ end;
 
 procedure TMyTestCase.TestDecode;
 begin
-  CheckEqualsString('This is' + #13 + #10 + 'multiline' + #13 + #10 + 'text',
+  AssertEquals('This is' + #13 + #10 + 'multiline' + #13 + #10 + 'text',
         DecodeControlCharacters('This is\r\nmultiline\r\ntext'));
-  CheckEqualsString('lorem' + #9 + 'ipsum' + #13 + #10 + 'dolor sit\amet' + #13 + #10,
+  AssertEquals('lorem' + #9 + 'ipsum' + #13 + #10 + 'dolor sit\amet' + #13 + #10,
         DecodeControlCharacters('lorem\tipsum\r\ndolor sit\\amet\r\n'));
-  CheckEqualsString('', DecodeControlCharacters(''));
-  CheckEqualsString('blablabla', DecodeControlCharacters('blablabla'));
+  AssertEquals('', DecodeControlCharacters(''));
+  AssertEquals('blablabla', DecodeControlCharacters('blablabla'));
 end;
 
 procedure TMyTestCase.TestFormatPath;
 {var
   CompName, UserName: String;}
 begin
-  CheckEqualsString('19-04-2020', GetFormattedPath('%D-%M-%Y'));
-  CheckEqualsString('2020_04_19', GetFormattedPath('%Y_%M_%D'));
-  CheckEqualsString('12.34.56', GetFormattedPath('%H.%N.%S'));
-  CheckNotEqualsString('12.34.56', GetFormattedPath('%H.%M.%S'),
-      'Month and minute mixed up');
-  CheckEquals('screenshot123', GetFormattedPath('screenshot123'),
-      'No format variables');
-  CheckEqualsString('2020\04-19\screenshot_20200419_123456.png',
-      GetFormattedPath('%Y\%M-%D\screenshot_%Y%M%D_%H%N%S.png'),
-      'Complex test');
-  CheckEqualsString('ÒÍËÌ¯ÓÚ 123456.jpeg',
-      GetFormattedPath('ÒÍËÌ¯ÓÚ %H%N%S.jpeg'),
-      'Non ASCII symbols');
-  CheckEqualsString('20200419_123456/123456-19042020', GetFormattedPath('%Y%M%D_%H%N%S/%H%N%S-%D%M%Y'));
+  AssertEquals('19-04-2020', GetFormattedPath('%D-%M-%Y'));
+  AssertEquals('2020_04_19', GetFormattedPath('%Y_%M_%D'));
+  AssertEquals('12.34.56', GetFormattedPath('%H.%N.%S'));
+  if '12.34.56' = GetFormattedPath('%H.%M.%S') then
+    Fail('Month and minute mixed up');
+  AssertEquals('No format variables',
+      'screenshot123', GetFormattedPath('screenshot123'));
+  AssertEquals('Complex test',
+      '2020\04-19\screenshot_20200419_123456.png',
+      GetFormattedPath('%Y\%M-%D\screenshot_%Y%M%D_%H%N%S.png'));
+  AssertEquals('Non ASCII symbols',
+      '—Å–∫—Ä–∏–Ω—à–æ—Ç 123456.jpeg',
+      GetFormattedPath('—Å–∫—Ä–∏–Ω—à–æ—Ç %H%N%S.jpeg'));
+  AssertEquals('20200419_123456/123456-19042020', GetFormattedPath('%Y%M%D_%H%N%S/%H%N%S-%D%M%Y'));
 
   // ToDo: Test incorect strings
 
   {// Note: On your machine this values will be different
   CompName := 'PC';
   UserName := 'Artem';
-  CheckEqualsString(CompName + '_' + UserName + '_image.tiff',
-      GetFormattedPath('%COMP_%USER_image.tiff'),
-      'PC and user name');}
+  AssertEquals('PC and user name',
+      CompName + '_' + UserName + '_image.tiff',
+      GetFormattedPath('%COMP_%USER_image.tiff'));}
 end;
 
 procedure TMyTestCase.TestInt2Str;
 begin
-  CheckEqualsString('152', Int2Str(152));
-  CheckEqualsString('123456789', Int2Str(123456789));
-  CheckEqualsString('-200300', Int2Str(-200300));
-  CheckEqualsString('007', Int2Str(7, 3));
-  CheckEqualsString('-00121', Int2Str(-121, 5));
-  CheckEqualsString('0', Int2Str(0));
-  CheckEqualsString('000000', Int2Str(0, 6));
-  CheckEqualsString('303', Int2Str(303, 3));
-  CheckEqualsString('-505', Int2Str(-505, 3));
+  AssertEquals('152', Int2Str(152));
+  AssertEquals('123456789', Int2Str(123456789));
+  AssertEquals('-200300', Int2Str(-200300));
+  AssertEquals('007', Int2Str(7, 3));
+  AssertEquals('-00121', Int2Str(-121, 5));
+  AssertEquals('0', Int2Str(0));
+  AssertEquals('000000', Int2Str(0, 6));
+  AssertEquals('303', Int2Str(303, 3));
+  AssertEquals('-505', Int2Str(-505, 3));
 end;
 
 procedure TMyTestCase.TestJoinPath;
 begin
   //CheckEqualsString('', JoinPath('', ''));
   {$IFDEF MSWINDOWS}
-  CheckEqualsString('c:\root\subdir\', JoinPath('c:\root', 'subdir\'));
-  CheckEqualsString('d:\folder1\folder2\folder3\folder4\',
-    JoinPath('d:\folder1\folder2\', '\folder3\folder4\'), 'Path starts with backslash');
-  CheckEqualsString('a:\DisketDir\', JoinPath('a:\DisketDir', ''), 'Empty path');
-  CheckEqualsString('mydir\picture.jpeg', JoinPath('mydir', 'picture.jpeg'),
-    'Relative path');
+  AssertEquals('c:\root\subdir\', JoinPath('c:\root', 'subdir\'));
+  AssertEquals('Path starts with backslash',
+    'd:\folder1\folder2\folder3\folder4\',
+    JoinPath('d:\folder1\folder2\', '\folder3\folder4\'));
+  AssertEquals('Empty path', 'a:\DisketDir\', JoinPath('a:\DisketDir', ''));
+  AssertEquals('Relative path',
+      'mydir\picture.jpeg', JoinPath('mydir', 'picture.jpeg'));
   {$ENDIF}
 end;
 
@@ -98,13 +106,15 @@ procedure TMyTestCase.TestRemoveExtraPathDelimiters;
 begin
   //CheckEqualsString('', RemoveExtraPathDelimiters(''));
   {$IFDEF MSWINDOWS}
-  CheckEqualsString('c:\dir1\dir2\dir3\file.txt', RemoveExtraPathDelimiters('c:\dir1\\dir2\\\dir3\\\\file.txt'));
-  CheckEqualsString('folder\', RemoveExtraPathDelimiters('folder\\'));
-  CheckEqualsString('\my folder\', RemoveExtraPathDelimiters('\\my folder\\'));
-  CheckEqualsString('d:\dir\file.txt', RemoveExtraPathDelimiters('d:\dir\file.txt'), 'Nothing to do');
+  AssertEquals('c:\dir1\dir2\dir3\file.txt', RemoveExtraPathDelimiters('c:\dir1\\dir2\\\dir3\\\\file.txt'));
+  AssertEquals('folder\', RemoveExtraPathDelimiters('folder\\'));
+  AssertEquals('\my folder\', RemoveExtraPathDelimiters('\\my folder\\'));
+  AssertEquals('Nothing to do', 'd:\dir\file.txt', RemoveExtraPathDelimiters('d:\dir\file.txt'));
   {$ENDIF}
 end;
 
 initialization
-  TestFramework.RegisterTest(TMyTestCase.Suite);
+
+  RegisterTest(TMyTestCase);
 end.
+
