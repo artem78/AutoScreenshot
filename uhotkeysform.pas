dad3e00c6fddeff22fa297e526446f0c9d8ca2a4
artem <megabyte1024@ya.ru>
2022-06-05 16:17:42 +0300

    Make THotKeyControl

--- uhotkeysform.pas
+++ uhotkeysform.pas
@@ -11,20 +11,38 @@ uses
 
 type
 
+  { THotKeyControl }
+
+  THotKeyControl = class(TCustomControl)
+  private
+    // Embeded components
+    AltCheckBox: TCheckBox;
+    CtrlCheckBox: TCheckBox;
+    ShiftCheckBox: TCheckBox;
+    KeyComboBox: TComboBox;
+
+    procedure SetHotKey(const AHotKey: THotKey);
+    function GetHotKey: THotKey;
+
+    procedure FillKeyComboBox;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+    property HotKey: THotKey read GetHotKey write SetHotKey;
+  end;
+
   { THotKeysForm }
 
   THotKeysForm = class(TForm)
     ButtonPanel: TButtonPanel;
-    StartStopAutoCaptureKeyComboBox: TComboBox;
-    StartStopAutoCaptureAltCheckBox: TCheckBox;
-    StartStopAutoCaptureShiftCheckBox: TCheckBox;
-    StartStopAutoCaptureCtrlCheckBox: TCheckBox;
-    StartStopAutoCaptureLabel: TLabel;
     procedure FormCreate(Sender: TObject);
   private
-    procedure SetStartStopAutoCaptureKey(const AKey: THotKey);
+    StartStopAutoCaptureLabel: TLabel;
+    StartStopAutoCaptureHotKeyControl: THotKeyControl;
+
+    procedure SetStartStopAutoCaptureKey(const AHotKey: THotKey);
     function GetStartStopAutoCaptureKey: THotKey;
-    procedure FillKeysComboBox;
   public
     property StartStopAutoCaptureKey: THotKey read GetStartStopAutoCaptureKey
                          write SetStartStopAutoCaptureKey;
@@ -40,61 +58,147 @@ uses
 
 {$R *.lfm}
 
-{ THotKeysForm }
+{ THotKeyControl }
 
-procedure THotKeysForm.FormCreate(Sender: TObject);
-begin
-  FillKeysComboBox;
-end;
-
-procedure THotKeysForm.SetStartStopAutoCaptureKey(const AKey: THotKey);
+procedure THotKeyControl.SetHotKey(const AHotKey: THotKey);
 var
   Idx: Integer;
 begin
-  StartStopAutoCaptureAltCheckBox.Checked := ssAlt in AKey.ShiftState;
-  StartStopAutoCaptureCtrlCheckBox.Checked := ssCtrl in AKey.ShiftState;
-  StartStopAutoCaptureShiftCheckBox.Checked := ssShift in AKey.ShiftState;
+  AltCheckBox.Checked := ssAlt in AHotKey.ShiftState;
+  CtrlCheckBox.Checked := ssCtrl in AHotKey.ShiftState;
+  ShiftCheckBox.Checked := ssShift in AHotKey.ShiftState;
 
-  Idx := StartStopAutoCaptureKeyComboBox.Items.IndexOfObject(TObject(Integer(AKey.Key)));
-  StartStopAutoCaptureKeyComboBox.ItemIndex := Idx;
+  Idx := KeyComboBox.Items.IndexOfObject(TObject(Integer(AHotKey.Key)));
+  KeyComboBox.ItemIndex := Idx;
 end;
 
-function THotKeysForm.GetStartStopAutoCaptureKey: THotKey;
+function THotKeyControl.GetHotKey: THotKey;
 var
   SelIdx: Integer;
 begin
   Result.ShiftState := [];
 
-  if StartStopAutoCaptureAltCheckBox.Checked then
+  if AltCheckBox.Checked then
     Include(Result.ShiftState, ssAlt);
 
-  if StartStopAutoCaptureCtrlCheckBox.Checked then
+  if CtrlCheckBox.Checked then
     Include(Result.ShiftState, ssCtrl);
 
-  if StartStopAutoCaptureShiftCheckBox.Checked then
+  if ShiftCheckBox.Checked then
     Include(Result.ShiftState, ssShift);
 
-  SelIdx := StartStopAutoCaptureKeyComboBox.ItemIndex;
-  Result.Key := Integer(StartStopAutoCaptureKeyComboBox.Items.Objects[SelIdx]);
+  SelIdx := KeyComboBox.ItemIndex;
+  Result.Key := Integer(KeyComboBox.Items.Objects[SelIdx]);
 end;
 
-procedure THotKeysForm.FillKeysComboBox;
+procedure THotKeyControl.FillKeyComboBox;
 var
   Key: Word;
   KeyName: String;
 begin
-  StartStopAutoCaptureKeyComboBox.Clear;
-  StartStopAutoCaptureKeyComboBox.Items.BeginUpdate;
+  KeyComboBox.Clear;
+  KeyComboBox.Items.BeginUpdate;
 
   for Key := VK_BACK to VK_SCROLL do
   // VK_BROWSER_BACK to VK_OEM_CLEAR
   begin
     KeyName := KeyAndShiftStateToKeyString(Key, []);
     if not KeyStringIsIrregular(KeyName) then
-      StartStopAutoCaptureKeyComboBox.Items.AddObject(KeyName, TObject(Integer(Key)));
+      KeyComboBox.Items.AddObject(KeyName, TObject(Integer(Key)));
   end;
 
-  StartStopAutoCaptureKeyComboBox.Items.EndUpdate;
+  KeyComboBox.Items.EndUpdate;
+end;
+
+constructor THotKeyControl.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  with GetControlClassDefaultSize do   // ???
+    SetInitialBounds(0, 0, CX, CY);
+
+  AltCheckBox := TCheckBox.Create(Self);
+  with AltCheckBox do
+  begin
+    Parent := Self;
+    //SetSubComponent(true);
+    Caption := 'Alt';
+    //Name := '...';
+  end;
+
+  CtrlCheckBox := TCheckBox.Create(Self);
+  with CtrlCheckBox do
+  begin
+    Parent := Self;
+    //SetSubComponent(true);
+    Caption := 'Ctrl';
+    //Name := '...';
+  end;
+
+  ShiftCheckBox := TCheckBox.Create(Self);
+  with ShiftCheckBox do
+  begin
+    Parent := Self;
+    //SetSubComponent(true);
+    Caption := 'Shift';
+    //Name := '...';
+  end;
+
+  KeyComboBox := TComboBox.Create(Self);
+  with KeyComboBox do
+  begin
+    Parent := Self;
+    //SetSubComponent(true);
+    Constraints.MinWidth := 100;
+    //Name := '...';
+    Style := csDropDownList;
+    //AutoSize := True;
+  end;
+    FillKeyComboBox;
+
+  ChildSizing.Layout := cclLeftToRightThenTopToBottom;
+  ChildSizing.ControlsPerLine := 4;
+  ChildSizing.HorizontalSpacing := 8;
+  AutoSize := True;
+
+end;
+
+destructor THotKeyControl.Destroy;
+begin
+  //KeyComboBox.Free;
+  //ShiftCheckBox.Free;
+  //CtrlCheckBox.Free;
+  //AltCheckBox.Free;
+
+  inherited Destroy;
+end;
+
+{ THotKeysForm }
+
+procedure THotKeysForm.FormCreate(Sender: TObject);
+begin
+  StartStopAutoCaptureLabel := TLabel.Create(Self);
+  with StartStopAutoCaptureLabel do
+  begin
+    Caption := 'Start/stop auto capture:';
+    Parent := Self;
+  end;
+
+  StartStopAutoCaptureHotKeyControl := THotKeyControl.Create(Self);
+  with StartStopAutoCaptureHotKeyControl do
+  begin
+    Parent := Self;
+  end;
+end;
+
+procedure THotKeysForm.SetStartStopAutoCaptureKey(const AHotKey: THotKey);
+begin
+  StartStopAutoCaptureHotKeyControl.HotKey := AHotKey;
+end;
+
+function THotKeysForm.GetStartStopAutoCaptureKey: THotKey;
+begin
+  Result := StartStopAutoCaptureHotKeyControl.HotKey;
 end;
 
 end.
