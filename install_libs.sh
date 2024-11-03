d1ee8029fa28873c2c55e42867cbc6156be2f033
artem78 <megabyte1024@ya.ru>
2024-11-03 11:55:57 +0300

    draft

---
+++ install_libs.sh
@@ -0,0 +1,15 @@
+# Для fpcupdeluxe
+sudo apt install build-essential libx11-dev libgtk2.0-dev libgdk-pixbuf2.0-dev libcairo2-dev libpango1.0-dev
+
+
+
+sudo apt-get install libsqlite3-dev libssl-dev
+
+# Для сборки ZIP
+sudo apt-get install ant ant-contrib
+
+# Для LazPackager (сборка DEB) требуется утилита debuild
+sudo apt install devscripts
+
+# Для запуска программы
+libsqlite3
